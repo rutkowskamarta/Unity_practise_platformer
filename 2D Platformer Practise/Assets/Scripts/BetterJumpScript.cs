33104ed65ec7eb29f47aec175766752ed7df9e61
marta.rutkowska <marta.rutkowska@protonmail.com>
2019-01-29 00:27:56 +0100

    basic player controller

---
+++ 2D Platformer Practise/Assets/Scripts/BetterJumpScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterJumpScript : MonoBehaviour {
+    [SerializeField] private float jumpMultiplier = 2f;
+    [SerializeField] private float fallMultiplier = 3f;
+
+    private Rigidbody2D playerRigidbody;
+
+	void Start ()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();	
+	}
+	
+	void Update ()
+    {
+        BetterJump();
+    }
+
+    private void BetterJump()
+    {
+        if (playerRigidbody.velocity.y > 0)
+        {
+            playerRigidbody.velocity += Vector2.up * Physics.gravity.y * jumpMultiplier*Time.deltaTime;
+        }
+        else if (playerRigidbody.velocity.y < 0)
+        {
+            playerRigidbody.velocity += Vector2.up * Physics.gravity.y * fallMultiplier * Time.deltaTime;
+
+        }
+    }
+}
---
+++ 2D Platformer Practise/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 1;
+    [SerializeField] private float jumpSpeed = 1;
+    [SerializeField] private float jumpTime = 0.5f;
+    [SerializeField] private AnimationClip playerAnimationRight;
+    
+    
+    private Rigidbody2D playerRigidbody;
+    private SpriteRenderer playerSpriteRenderer;
+    private bool isGrounded = false;
+    private bool isJumping = false;
+    private float jumpTimeCounter = 0;
+    private Animation playerAnimation;
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
+        playerAnimation = GetComponent<Animation>();
+        
+    }
+
+    void FixedUpdate()
+    {
+        PlayerMovement();
+        PlayerStopMove();
+        PlayerJump();
+    }
+
+    private void PlayerMovement()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerRigidbody.velocity = new Vector2(-movementSpeed, playerRigidbody.velocity.y);
+            playerSpriteRenderer.flipX = true;
+
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerRigidbody.velocity = new Vector2(movementSpeed, playerRigidbody.velocity.y);
+            playerSpriteRenderer.flipX = false;
+
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+
+    private void PlayerStopMove()
+    {
+        if ((Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D)))
+        {
+            playerRigidbody.velocity = Vector2.zero;
+        }
+    }
+
+    private void PlayerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpSpeed);
+            isGrounded = false;
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            JumpRotationAnimation();
+        }
+        PlayerLongerJump();
+    }
+
+    private void PlayerLongerJump()
+    {
+        if (Input.GetKey(KeyCode.Space) && isJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpSpeed);
+                isGrounded = false;
+                jumpTimeCounter -= Time.deltaTime;
+                
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void JumpRotationAnimation()
+    {
+        playerAnimation.clip = playerAnimationRight;
+        playerAnimation.Play();
+    }
+}
